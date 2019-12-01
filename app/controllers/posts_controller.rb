0151c7d98da639e99f4dd0d650414dc7c695d559
github username <github email address>
2019-12-01 15:11:04 -0800

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,12 +5,16 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.new
   end
 
   def update
     @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.valid?
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
